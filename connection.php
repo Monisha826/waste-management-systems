4b65f144c63661132664e6307f1c868b323a7747
Monisha L <monishareddy28082001@gmail.com>
2025-08-20 20:20:04 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,4 @@
 <?php 
 $con = mysqli_connect('db', 'root', 'root', 'wms');
-if (!$con) {
-    die("❌ Connection failed: " . mysqli_connect_error());
-} else {
-    echo "✅ Connected to MySQL successfully!";
-}
 ?>
+
